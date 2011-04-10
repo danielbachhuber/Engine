0671dea93ad2e4faa867de63024c14c245161596
Daniel Bachhuber <d@danielbachhuber.com>
2011-04-09 20:01:31 -0400

    Post format support

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+
+if ( !class_exists( 'dub' ) ) {
+
+class dub {
+	
+	/**
+	 * __construct()
+	 */
+	function __construct() {
+		
+		// Add support for post formats
+		
+		add_hook( 'after_setup_theme', array( &$this, 'add_post_formats' ) );
+		
+	} // END __construct()
+	
+	/**
+	 * init()
+	 */
+	function init() {
+		
+	} // END init()
+	
+	/**
+	 * add_post_formats()
+	 */
+	function add_post_formats() {
+		
+		$post_formats = array(
+			'aside',
+			'gallery',
+			'status',
+			'quote',
+			'image',
+			'link',
+		);
+		add_theme_support( 'post-formats', $post_formats );
+		add_post_type_support( 'post', 'post-formats' );
+		
+	} // END add_post_formats()
+	
+	
+} // END class dub
+	
+} // END if ( !class_exists( 'dub' ) )
+
+
+?>
