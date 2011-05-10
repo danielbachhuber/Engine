f67c422f6b15ba5181818110e54c35f81e533602
Daniel Bachhuber <d@danielbachhuber.com>
2011-05-10 19:15:28 -0400

    Consolidate all of the loop parts

--- loop-single.php
+++ loop-single.php
@@ -7,70 +7,16 @@
 	<?php if ( 'status' == dub_get_post_format() ) : ?>
 
 	<div class="post post-format-status">
-		
-		<div class="entry">
-			<?php the_content(); ?>
-		</div>
-		
-		<div class="meta has-avatar">
-			<div class="avatar float-left">
-			<?php echo get_avatar( get_option('admin_email'), 48 ); ?>
-			</div>
-			<div class="primary-info">
-				<span class="author"><?php the_author_link(); ?></span>
-				<span class="timestamp"><?php dub_timestamp(); ?></span>
-				<span class="tags"><?php the_tags( '', ', ', '' ); ?></span>				
-			</div>
-			<div class="clear-both"></div>			
-		</div>
-		
-	</div><!-- END .post -->
 	
 	<?php elseif ( 'aside' == dub_get_post_format() ) : ?>
 		
 	<div class="post post-format-aside">
-
-		<div class="entry">
-			<?php the_content(); ?>
-		</div>
-
-		<div class="meta has-avatar">
-			<div class="avatar float-left">
-			<?php echo get_avatar( get_option('admin_email'), 48 ); ?>
-			</div>
-			<div class="primary-info">
-				<span class="author"><?php the_author_link(); ?></span>
-				<span class="timestamp"><a href="<?php the_permalink(); ?>"><?php dub_timestamp(); ?></a></span>
-				<span class="tags"><?php the_tags( '', ', ', '' ); ?></span>				
-			</div>
-			<div class="clear-both"></div>			
-		</div>
-
-	</div><!-- END .post -->	
 	
 	<?php elseif ( 'photo' == dub_get_post_format() ) : ?>
 		
 	<div class="post post-format-photo">
 		
 		<h2 class="post-title"><?php the_title(); ?></h2>
-
-		<div class="entry">
-			<?php the_content(); ?>
-		</div>
-		
-		<div class="meta has-avatar">
-			<div class="avatar float-left">
-			<?php echo get_avatar( get_option('admin_email'), 48 ); ?>
-			</div>
-			<div class="primary-info">
-				<span class="author"><?php the_author_link(); ?></span>
-				<span class="timestamp"><a href="<?php the_permalink(); ?>"><?php dub_timestamp(); ?></a></span>
-				<span class="tags"><?php the_tags( '', ', ', '' ); ?></span>				
-			</div>
-			<div class="clear-both"></div>			
-		</div>
-
-	</div><!-- END .post -->
 	
 	<?php else: ?>
 		
@@ -83,26 +29,26 @@
 			<?php the_excerpt(); ?>
 		</div>
 		<?php endif; ?>
-		
-		<div class="entry">
-			<?php the_content(); ?>
-		</div>
-		
-		<div class="meta has-avatar">
-			<div class="avatar float-left">
-			<?php echo get_avatar( get_option('admin_email'), 48 ); ?>
-			</div>
-			<div class="primary-info">
-				<span class="author"><?php the_author_link(); ?></span>
-				<span class="timestamp"><a href="<?php the_permalink(); ?>"><?php dub_timestamp(); ?></a></span>
-				<span class="tags"><?php the_tags( '', ', ', '' ); ?></span>				
-			</div>
-			<div class="clear-both"></div>			
-		</div>		
-
-	</div><!-- END .post -->	
 	
 	<?php endif; ?>
+	
+	<div class="entry">
+		<?php the_content(); ?>
+	</div>
+	
+	<div class="meta has-avatar">
+		<div class="avatar float-left">
+		<?php echo get_avatar( get_option('admin_email'), 48 ); ?>
+		</div>
+		<div class="primary-info">
+			<span class="author"><?php the_author_link(); ?></span>
+			<span class="timestamp"><a href="<?php the_permalink(); ?>"><?php dub_timestamp(); ?></a></span>
+			<span class="tags"><?php the_tags( '', ', ', '' ); ?></span>				
+		</div>
+		<div class="clear-both"></div>			
+	</div>
+	
+	</div><!-- END .post -->
 
 <?php endwhile; ?>
 
